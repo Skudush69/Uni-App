6a80dd02be931d8b2e47c12462c0bc3b33eb0f51
Skudush69 <josemwangi0069@gmail.com>
2023-09-27 09:21:34 +0300

    fisrt commit

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -5,7 +5,9 @@
 <template>
     <div id = "landing page" >
       <h1>Welome to Uni App</h1>
-      <nav>
+    <body>
+      <section class="header">
+    <nav>
       <a href="index.html"><img src="images/logo.png"></a>
        <div class="nav-links" id="navlinks">
         <i class="fa fa-times" onclick="hideMenu()"></i>
@@ -19,5 +21,93 @@
       </div>
       <i class="fa fa-bars" onclick="showMenu()"></i>
     </nav>
+  <div class="text-box">
+    <h1>Beacon Hills University</h1>
+    <p>Welcome to Beacon Hills University where there is provision of an all-round quality<br>education where you get
+      professional training and acquire personal qualities.
+    </p>
+    <a href=""class="hero-btn">Visit Us To know More</a>
+  </div>  
+  
+  </section>
+
+<!------ Course ------>
+
+<section class="course">
+    <h1>Courses we Offer</h1>
+    <p>scienta potentia est.</p>
+
+    <div class="row">
+        <div class="course-col">
+            <h3>Intermediate</h3>
+            <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit.
+              Pellentesque aliquet turpis nulla,eleifend faucibus est sollicitudin ut.
+              Maecenas ut venenatis ex, et dapibus purus Donec sit.
+            </p>
+        </div>
+        <div class="course-col">
+          <h3>Degree</h3>
+          <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit.
+            Pellentesque aliquet turpis nulla,eleifend faucibus est sollicitudin ut.
+            Maecenas ut venenatis ex, et dapibus purus Donec sit.
+          </p>
+      </div>
+      <div class="course-col">
+        <h3>Post Graduation</h3>
+        <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit.
+          Pellentesque aliquet turpis nulla,eleifend faucibus est sollicitudin ut.
+          Maecenas ut venenatis ex, et dapibus purus Donec sit.
+        </p>
+    </div>
+    </div>
+
+</section>
+
+<!------ capmus ------>
+
+<section class="campus">
+  <h1>Global Campus</h1>
+  <p>scienta potentia est.</p>
+
+<div class="row">
+    <div class="campus-col">
+      <img src="images/london.png">
+      <div class="layer">
+        <h3>NAIROBI</h3>
+      </div>
+    </div>
+    <div class="campus-col">
+      <img src="images/newyork.png">
+      <div class="layer">
+        <h3>NAKURU</h3>
+      </div>
+    </div>
+    <div class="campus-col">
+      <img src="images/washington.png">
+      <div class="layer">
+        <h3>ELDORET</h3>
+      </div>
+    </div>
+</div>
+
+</section>
+
+
+
+
+
+<!------JavaScript for Toggle Menu------>
+  <script>
+
+      var navlinks=document.getElementById("navlinks");
+
+      function showMenu(){
+        navlinks.style.right ="0";  
+      }
+      function hideMenu(){
+        navlinks.style.right ="-200px";  
+      }
+  </script>
+  </body>
     </div>
 </template>
